28d2ab0a923dd545879044e6c3541b71ad1a7ec2
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-08-05 00:38:48 +0800

    ch5

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,7 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Text("Hello Nifad", style: TextStyle(fontSize: 24)),
         ),
-
-
-      ),
+        ),
     );
   }
 
