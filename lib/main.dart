60be9f008f85f1cf159de7418d2b382c5456d94e
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-08-05 00:24:58 +0800

    Learning Flutter

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Hello Nifad"), centerTitle: true),
+        body: Center(
+          child: Text("Hello Nifad", style: TextStyle(fontSize: 24)),
+        ),
+      ),
+    );
+  }
+}
